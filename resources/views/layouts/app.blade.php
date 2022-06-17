ee0bdce206b2c9ef8c173aa4495c6573151c9d04
Anesti Kolici <anestikolici@gmail.com>
2022-06-17 22:06:46 +0200

    warehouse project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,6 @@
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}" style="color: white">Login</a></li>
-                            <li><a href="{{ route('register') }}" style="color: white">Register</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" style="color: white">
